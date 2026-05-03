6725c5355ba6436975a0ba753a62645a41fc2c37
anonimo3967-alt <anonimo3967@gmail.com>
2026-05-03 15:44:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,11 @@ st.title("MQTT para enviar cosas en vez de recibirlas")
 
 st.text("Unas aclaraciones... Para que funcione, el cliente NO puede ser el mismo que el de wokwi, pero el topico de subscripcion si")
 
+st.text("Ahora una pequeña explicación de como funciona... Resulta que si le das a Encender los motores y luego escoges un valor en el slider, y le das al boton de enviar, tu valor se enviara a un MQTT para girar un motor en wokwi")
 
 
-if st.button('ON'):
+
+if st.button('Encender los motores', type="primary"):
     act1="ON"
     client1= paho.Client("cliente_amadeus_10229")                           
     client1.on_publish = on_publish                          
@@ -50,7 +52,7 @@ if st.button('ON'):
 else:
     st.write('')
 
-if st.button('OFF'):
+if st.button('Apagar los motores'):
     act1="OFF"
     client1= paho.Client("cliente_amadeus_10229")                           
     client1.on_publish = on_publish                          
